4a0361a861663b6ef5ec7e0e4f5db05582d07497
Seth McCauley <seth.a.mccauley@gmail.com>
2020-05-01 18:09:13 -0600

    Updates

---
+++ precache-manifest.ba77f1eaca5f91a6e21a4c9946667965.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b147bcc4515ae8308d75d8ae26ba716",
+    "url": "/nin-dmg/index.html"
+  },
+  {
+    "revision": "2c4695155c3a98ad8f74",
+    "url": "/nin-dmg/static/css/2.0fba4bd8.chunk.css"
+  },
+  {
+    "revision": "ae1ca40911bc9f08396f",
+    "url": "/nin-dmg/static/css/main.6cae427c.chunk.css"
+  },
+  {
+    "revision": "2c4695155c3a98ad8f74",
+    "url": "/nin-dmg/static/js/2.cb74f00b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nin-dmg/static/js/2.cb74f00b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1ca40911bc9f08396f",
+    "url": "/nin-dmg/static/js/main.c5f11fe0.chunk.js"
+  },
+  {
+    "revision": "fdd21b720b414249bc8f",
+    "url": "/nin-dmg/static/js/runtime-main.87168b30.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nin-dmg/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/nin-dmg/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nin-dmg/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nin-dmg/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nin-dmg/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c550c7b19c9f8dff73071fc227660017",
+    "url": "/nin-dmg/static/media/tarus.c550c7b1.png"
+  }
+]);
